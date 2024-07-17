dd7cd4f98cd9734a8c85f2f2134f073501d64f16
Marvin Ambrose <marvambi@gmail.com>
2024-07-17 07:59:21 +0100

    refactor: improves the copy for the messaging on the homepage

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,13 +7,13 @@ export default function Home() {
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
         <img
-          class="my-6"
-          src="/logo.svg"
+          class="my-6 rounded-md"
+          src="https://media.licdn.com/dms/image/C5103AQFnoeAMkAm8nw/profile-displayphoto-shrink_200_200/0/1516854313381?e=1726704000&v=beta&t=7BON5wKh-ZAba9tyR0fV9-cDiLMM5DeJveG7JQM41V0"
           width="128"
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to Fresh</h1>
+        <h1 class="text-4xl font-bold">Welcome to Perspectives</h1>
         <p class="my-4">
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
